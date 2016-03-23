20044ebbec4dbd9394823300e96327ee56439014
RyanDam <py.ryan.dam@gmail.com>
2016-03-23 23:33:22 +0700

    Initial Commit

---
+++ Twitbot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitbot
+//
+//  Created by Dam Vu Duy on 3/23/16.
+//  Copyright © 2016 dotRStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
